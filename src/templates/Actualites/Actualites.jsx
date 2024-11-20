f88040805071f496504cf55cd33f0e214fb0b814
sly <sylvaincadoret@hotmail.com>
2024-11-20 17:00:48 +0100

    actualites upgraded

--- src/templates/Actualites/Actualites.jsx
+++ src/templates/Actualites/Actualites.jsx
@@ -5,12 +5,14 @@ export default function Actualites() {
     <div className={styles.actualitesContainer}>
       <h1>Tendances & Actus</h1>
 
-      <iframe
-        width="850"
-        height="1000"
-        src="https://rss.app/embed/v1/list/eWawwOB0YQLWBQxp"
-        frameborder="0"
-      ></iframe>
+      <div className={styles.newsList}>
+        <iframe
+          width="850"
+          height="1600"
+          src="https://rss.app/embed/v1/list/eWawwOB0YQLWBQxp"
+          frameborder="0"
+        ></iframe>
+      </div>
     </div>
   );
 }
