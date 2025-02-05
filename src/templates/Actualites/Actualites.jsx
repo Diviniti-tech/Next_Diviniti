479f212c708ffe962254af7ec099440949e72a2f
sly <sylvaincadoret@hotmail.com>
2025-02-05 16:55:39 +0100

    backup

--- src/templates/Actualites/Actualites.jsx
+++ src/templates/Actualites/Actualites.jsx
@@ -1,22 +1,35 @@
+import { useState, useEffect } from "react";
 import styles from "./style.module.scss";
+import { MoonLoader } from "react-spinners";
 
 export default function Actualites() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Timeout de secours si onLoad ne fonctionne pas
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsLoading(false), 1000); // 5 sec max
+    return () => clearTimeout(timeout); // Nettoyage du timeout
+  }, []);
+
   return (
     <div className={styles.actualitesContainer}>
       <h1>Tendances & Actus</h1>
 
       <div className={styles.newsList}>
-        {/* <iframe
-          width="1200"
-          height="1600"
-          src="https://rss.app/embed/v1/wall/eWawwOB0YQLWBQxp"
-          frameborder="0"
-        ></iframe> */}
+        {/* Loader affiché tant que l'iframe n'est pas chargé */}
+        {isLoading && (
+          <div className={styles.loader}>
+            <MoonLoader color="#FFD700" loading={isLoading} size={90} />
+          </div>
+        )}
         <iframe
-          width="1200"
-          height="1000"
-          src="https://rss.app/embed/v1/list/eWawwOB0YQLWBQxp"
-          frameborder="0"
+        
+          width="900"
+          height="1400"
+          src="https://rss.app/embed/v1/wall/eWawwOB0YQLWBQxp"
+          frameBorder="0"
+          onLoad={() => setIsLoading(false)} // Masquer le loader une fois chargé
+          style={{ display: isLoading ? "none" : "block" }} // Masquer l'iframe tant qu'il n'est pas prêt
         ></iframe>
       </div>
     </div>
