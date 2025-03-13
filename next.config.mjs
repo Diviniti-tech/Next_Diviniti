1564694f4bb6fc6830fd2ba817899335e527e108
sly <sylvaincadoret@hotmail.com>
2025-03-13 12:28:51 +0100

    deployed

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,14 @@
-// // next.config.mjs
-// export default {
-//   output: 'export', // Utiliser 'output: export' pour générer des pages statiques
-//   images: {
-//     unoptimized: true, // Désactiver l'optimisation des images pour l'export statique
-//   },
-// };
-
-const isProd = process.env.NODE_ENV === 'production';
-
+// next.config.mjs
 export default {
-  ...(isProd && { output: 'export' }), // Appliquer seulement en prod
+  output: 'export', // Utiliser 'output: export' pour générer des pages statiques
   images: {
-    unoptimized: true, // Désactiver l'optimisation des images pour le mode statique
+    unoptimized: true, // Désactiver l'optimisation des images pour l'export statique
   },
 };
+
+// const isProd = process.env.NODE_ENV === 'production';
+
+// export default {
+//   ...(isProd && { output: 'export' }), // Appliquer seulement en prod
+
+// };
