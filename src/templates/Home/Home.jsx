76298daa1a765fc73d190f85d9d54a5efddeeaa4
sly <sylvaincadoret@hotmail.com>
2025-03-13 11:34:18 +0100

    pages responsive updated

--- src/templates/Home/Home.jsx
+++ src/templates/Home/Home.jsx
@@ -1,120 +1,120 @@
-import { useState } from "react";
-import styles from "./style.module.scss";
-import Link from "next/link";
-import { motion } from "framer-motion";
-import Head from "next/head";
-// import WebsiteIntro from "@/components/WebsiteIntro/websiteIntro";
-// import ModalDownloadPdf from "@/components/ModalDownloadPdf/ModalDownloadPdf";
-import ModalBookDemo from "@/components/ModalBookDemo/ModalBookDemo";
+// import { useState } from "react";
+// import styles from "./style.module.scss";
+// import Link from "next/link";
+// import { motion } from "framer-motion";
+// import Head from "next/head";
+// // import WebsiteIntro from "@/components/WebsiteIntro/websiteIntro";
+// // import ModalDownloadPdf from "@/components/ModalDownloadPdf/ModalDownloadPdf";
+// import ModalBookDemo from "@/components/ModalBookDemo/ModalBookDemo";
 
-export default function Home() {
-  const container = {
-    hidden: { opacity: 0, scale: 0 },
-    visible: {
-      opacity: 1,
-      scale: 1,
-      transition: {
-        delayChildren: 0.4,
-        staggerChildren: 0.6,
-      },
-    },
-  };
+// export default function Home() {
+//   const container = {
+//     hidden: { opacity: 0, scale: 0 },
+//     visible: {
+//       opacity: 1,
+//       scale: 1,
+//       transition: {
+//         delayChildren: 0.4,
+//         staggerChildren: 0.6,
+//       },
+//     },
+//   };
 
-  const item = {
-    hidden: { y: 20, opacity: 0 },
-    visible: {
-      y: 0,
-      opacity: 1,
-    },
-  };
+//   const item = {
+//     hidden: { y: 20, opacity: 0 },
+//     visible: {
+//       y: 0,
+//       opacity: 1,
+//     },
+//   };
 
-  // const DownloadPdfButton = () => (
-  //   <motion.button
-  //     whileHover={{ scale: 1.05 }}
-  //     className={styles.downloadButton}
-  //     onClick={() => setShowModal(true)}
-  //   >
-  //     Télécharger la plaquette
-  //   </motion.button>
-  // );
+//   // const DownloadPdfButton = () => (
+//   //   <motion.button
+//   //     whileHover={{ scale: 1.05 }}
+//   //     className={styles.downloadButton}
+//   //     onClick={() => setShowModal(true)}
+//   //   >
+//   //     Télécharger la plaquette
+//   //   </motion.button>
+//   // );
 
-  const [showModal, setShowModal] = useState(false);
+//   const [showModal, setShowModal] = useState(false);
 
-  return (
-    <>
-      <Head>
-        <title>Diviniti - Sûreté augmentée pour la ville de demain</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          property="og:title"
-          content="Diviniti - Sûreté augmentée pour la ville de demain"
-        />
-        <meta
-          property="og:description"
-          content="Découvrez les solutions Diviniti pour la ville de demain, combinant IA et réalité étendue pour une sécurité proactive."
-        />
-        <meta property="og:url" content="https://www.diviniti.tech" />
-      </Head>
+//   return (
+//     <>
+//       <Head>
+//         <title>Diviniti - Sûreté augmentée pour la ville de demain</title>
+//         <meta name="viewport" content="width=device-width, initial-scale=1" />
+//         <meta
+//           property="og:title"
+//           content="Diviniti - Sûreté augmentée pour la ville de demain"
+//         />
+//         <meta
+//           property="og:description"
+//           content="Découvrez les solutions Diviniti pour la ville de demain, combinant IA et réalité étendue pour une sécurité proactive."
+//         />
+//         <meta property="og:url" content="https://www.diviniti.tech" />
+//       </Head>
 
-      <motion.div
-        className={styles.homeContainer}
-        initial="hidden"
-        animate="visible"
-        variants={container}
-      >
-        {/* <motion.img
-          src="/assets/diviniti-logo-white.svg"
-          alt="Diviniti logo"
-          className={styles.homeLogo}
-          // animate={{ scale: [1, 1.05, 1] }}
-          // transition={{
-          //   duration: 1,
-          //   ease: "easeInOut",
-          //   repeat: Infinity,
-          // }}
-        /> */}
-        <motion.div className={styles.titles} variants={container}>
+//       <motion.div
+//         className={styles.homeContainer}
+//         initial="hidden"
+//         animate="visible"
+//         variants={container}
+//       >
+//         {/* <motion.img
+//           src="/assets/diviniti-logo-white.svg"
+//           alt="Diviniti logo"
+//           className={styles.homeLogo}
+//           // animate={{ scale: [1, 1.05, 1] }}
+//           // transition={{
+//           //   duration: 1,
+//           //   ease: "easeInOut",
+//           //   repeat: Infinity,
+//           // }}
+//         /> */}
+//         <motion.div className={styles.titles} variants={container}>
 
-          <motion.h2 variants={item}>Gaming for Realiti</motion.h2>
-          <motion.h1 variants={item}>
-            Le nouveau système d&apos;exploitation sûreté
-          </motion.h1>
+//           <motion.h2 variants={item}>Gaming for Realiti</motion.h2>
+//           <motion.h1 variants={item}>
+//             Le nouveau système d&apos;exploitation sûreté
+//           </motion.h1>
 
-          <iframe className={styles.video}
+//           <iframe className={styles.video}
           
-            src="https://www.youtube.com/embed/WR8Izjm4JF8"
-            title="Diviniti: Révolutionnez la sûreté avec l&#39;IA, la XR et les codes du gaming"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
-          ></iframe>
+//             src="https://www.youtube.com/embed/WR8Izjm4JF8"
+//             title="Diviniti: Révolutionnez la sûreté avec l&#39;IA, la XR et les codes du gaming"
+//             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+//             referrerpolicy="strict-origin-when-cross-origin"
+//             allowfullscreen
+//           ></iframe>
 
-          <motion.div variants={item} className={styles.cta}>
-            <motion.div whileHover={{ scale: 1.05 }}>
-              <Link
-                href="/gaming-for-realiti"
-                passHref
-                className={styles.ctaButton}
-              >
-                <button> En savoir plus</button>
-              </Link>
-            </motion.div>
+//           <motion.div variants={item} className={styles.cta}>
+//             <motion.div whileHover={{ scale: 1.05 }}>
+//               <Link
+//                 href="/gaming-for-realiti"
+//                 passHref
+//                 className={styles.ctaButton}
+//               >
+//                 <button> En savoir plus</button>
+//               </Link>
+//             </motion.div>
 
-            <motion.div
-              whileHover={{ scale: 1.05 }}
-              onClick={() => setShowModal(true)}
-            >
-              {" "}
-              <button className={styles.ctaButton}>Demander une démo </button>
-            </motion.div>
-            {/* Composant bouton de téléchargement de la plaquette commerciale */}
-            {/* <DownloadPdfButton /> */}
-          </motion.div>
-        </motion.div>
-        {/* Composant Modal pour télécharger la plaquette commerciale */}
-        {/* <ModalDownloadPdf showModal={showModal} setShowModal={setShowModal} /> */}
-        <ModalBookDemo showModal={showModal} setShowModal={setShowModal} />
-      </motion.div>
-    </>
-  );
-}
+//             <motion.div
+//               whileHover={{ scale: 1.05 }}
+//               onClick={() => setShowModal(true)}
+//             >
+//               {" "}
+//               <button className={styles.ctaButton}>Demander une démo </button>
+//             </motion.div>
+//             {/* Composant bouton de téléchargement de la plaquette commerciale */}
+//             {/* <DownloadPdfButton /> */}
+//           </motion.div>
+//         </motion.div>
+//         {/* Composant Modal pour télécharger la plaquette commerciale */}
+//         {/* <ModalDownloadPdf showModal={showModal} setShowModal={setShowModal} /> */}
+//         <ModalBookDemo showModal={showModal} setShowModal={setShowModal} />
+//       </motion.div>
+//     </>
+//   );
+// }
--- src/templates/Team/Team.jsx
+++ src/templates/Team/Team.jsx
@@ -1,117 +1,117 @@
-import styles from "./style.module.scss";
-import Image from "next/image";
-import { motion, useInView } from "framer-motion";
+// import styles from "./style.module.scss";
+// import Image from "next/image";
+// import { motion, useInView } from "framer-motion";
 
-export default function Team() {
-  const team = [
-    {
-      name: "Fabien THOMAS",
-      role: "CXO",
-      image: "/assets/team/FABIEN.jpg",
-    },
+// export default function Team() {
+//   const team = [
+//     {
+//       name: "Fabien THOMAS",
+//       role: "CXO",
+//       image: "/assets/team/FABIEN.jpg",
+//     },
 
-    {
-      name: "Stecie RAZA",
-      role: "Chargée de Communication & Marketing",
-      image: "/assets/team/STECIE_.jpg",
-    },
-    {
-      name: "Abdel-Rahmen DEGHBOUDJ",
-      role: "Développeur",
-      image:
-        "/assets/team/ABDEL-RAHMEN_.webp",
-    },
+//     {
+//       name: "Stecie RAZA",
+//       role: "Chargée de Communication & Marketing",
+//       image: "/assets/team/STECIE_.jpg",
+//     },
+//     {
+//       name: "Abdel-Rahmen DEGHBOUDJ",
+//       role: "Développeur",
+//       image:
+//         "/assets/team/ABDEL-RAHMEN_.webp",
+//     },
     
-    {
-      name: "Andy BARZOLA",
-      role: "3D Designer",
-      image: "/assets/team/ANDY.webp",
-    },
-    {
-      name: "Augustin MAHIEU",
-      role: "Développeur",
-      image: "/assets/team/AUGUSTIN.jpg",
-    },
+//     {
+//       name: "Andy BARZOLA",
+//       role: "3D Designer",
+//       image: "/assets/team/ANDY.webp",
+//     },
+//     {
+//       name: "Augustin MAHIEU",
+//       role: "Développeur",
+//       image: "/assets/team/AUGUSTIN.jpg",
+//     },
  
-    {
-      name: "Zine-Eddine BENZENATI",
-      role: "Développeur",
-      image:
-        "/assets/team/ZINE-EDDINE.webp",
-    },
-  ];
+//     {
+//       name: "Zine-Eddine BENZENATI",
+//       role: "Développeur",
+//       image:
+//         "/assets/team/ZINE-EDDINE.webp",
+//     },
+//   ];
 
-  return (
-    <div className={styles.teamContainer}>
-      <div className={styles.teamHero}>
-        <div className={styles.heroLeft}>
-          <h1>Diviniti : Qui Sommes-Nous ?</h1>
-          <p>
-            Chez Diviniti, notre force réside dans les talents exceptionnels qui
-            composent notre équipe. Ensemble, nous repoussons les limites de la
-            technologie pour imaginer, concevoir et réaliser des solutions
-            révolutionnaires pour la ville de demain. De la stratégie à la
-            conception, en passant par le développement et l’expérience
-            utilisateur, chaque membre de notre équipe joue un rôle essentiel
-            dans la création d’un avenir plus sûr, plus intelligent et plus
-            immersif.
-          </p>
-          <p>
-            Animés par une passion commune pour l’innovation, nous unissons nos
-            expertises pour transformer la manière dont le monde aborde la
-            sûreté et la supervision.
-          </p>
-        </div>
-        <motion.div className={styles.heroRight}
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1 }}
-        >
+//   return (
+//     <div className={styles.teamContainer}>
+//       <div className={styles.teamHero}>
+//         <div className={styles.heroLeft}>
+//           <h1>Diviniti : Qui Sommes-Nous ?</h1>
+//           <p>
+//             Chez Diviniti, notre force réside dans les talents exceptionnels qui
+//             composent notre équipe. Ensemble, nous repoussons les limites de la
+//             technologie pour imaginer, concevoir et réaliser des solutions
+//             révolutionnaires pour la ville de demain. De la stratégie à la
+//             conception, en passant par le développement et l’expérience
+//             utilisateur, chaque membre de notre équipe joue un rôle essentiel
+//             dans la création d’un avenir plus sûr, plus intelligent et plus
+//             immersif.
+//           </p>
+//           <p>
+//             Animés par une passion commune pour l’innovation, nous unissons nos
+//             expertises pour transformer la manière dont le monde aborde la
+//             sûreté et la supervision.
+//           </p>
+//         </div>
+//         <motion.div className={styles.heroRight}
+//           initial={{ opacity: 0, x: 100 }}
+//           animate={{ opacity: 1, x: 0 }}
+//           transition={{ duration: 1 }}
+//         >
         
-          <Image
-            src="/assets/team/YANN.jpg"
-            alt="team"
-            width={300}
-            height={250}
+//           <Image
+//             src="/assets/team/YANN.jpg"
+//             alt="team"
+//             width={300}
+//             height={250}
          
-          />
-          <h2>Yann DUCHET</h2>
-          <p>CEO</p>
-        </motion.div>
-      </div>
+//           />
+//           <h2>Yann DUCHET</h2>
+//           <p>CEO</p>
+//         </motion.div>
+//       </div>
 
-      <div className={styles.teamMembers}>
-        {team.map((member, index) => (
-          <motion.div
-            key={index}
-            className={styles.teamMember}
-            initial={{ opacity: 0, y: 100, x: index % 2 === 0 ? -100 : 100 }}
-            whileInView={{ opacity: 1, y: 0, x: 0 }}
-            viewport={{ once: true, amount: 0.2 }}
-            transition={{
-              duration: 0.9,
-              delay: index * 0.1,
-              type: "spring",
-              stiffness: 100,
-            }}
-            whileHover={{
-              scale: 1.03,
-              y: -10,
-              transition: { type: "spring", stiffness: 100 },
-            }}
+//       <div className={styles.teamMembers}>
+//         {team.map((member, index) => (
+//           <motion.div
+//             key={index}
+//             className={styles.teamMember}
+//             initial={{ opacity: 0, y: 100, x: index % 2 === 0 ? -100 : 100 }}
+//             whileInView={{ opacity: 1, y: 0, x: 0 }}
+//             viewport={{ once: true, amount: 0.2 }}
+//             transition={{
+//               duration: 0.9,
+//               delay: index * 0.1,
+//               type: "spring",
+//               stiffness: 100,
+//             }}
+//             whileHover={{
+//               scale: 1.03,
+//               y: -10,
+//               transition: { type: "spring", stiffness: 100 },
+//             }}
        
-          >
-            <Image
-              src={member.image}
-              alt={member.name}
-              width={150}
-              height={150}
-            />
-            <h2>{member.name}</h2>
-            <p>{member.role}</p>
-          </motion.div>
-        ))}
-      </div>
-    </div>
-  );
-}
+//           >
+//             <Image
+//               src={member.image}
+//               alt={member.name}
+//               width={150}
+//               height={150}
+//             />
+//             <h2>{member.name}</h2>
+//             <p>{member.role}</p>
+//           </motion.div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
